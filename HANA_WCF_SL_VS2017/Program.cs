32dd4201b0289b422dc0d7e1ac4fd723e8601c62
Suhut <suhut.wadiyo@gmail.com>
2020-02-15 14:54:42 +0700

    first commit

---
+++ HANA_WCF_SL_VS2017/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HANA_WCF_SL_VS2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
